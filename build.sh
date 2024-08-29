3adefd9a2e1964fd8cddb23a20a53ceb60537dbe
Caleb Meredith <calebmeredith8@gmail.com>
2024-08-29 12:17:58 -0400

    Update GitHub build action
    - Run GitHub action manually
    - Don’t require latest dependency versions
    - Build Linux arm64 in GitHub’s provided runners with QEMU
    - Install libtool for de265 ./autogen.sh script

--- build.sh
+++ build.sh
@@ -104,7 +104,12 @@ done
 for flavour in linux-x64 linuxmusl-x64 linux-armv6 linux-arm64v8 linuxmusl-arm64v8 linux-ppc64le linux-s390x; do
   if [ $PLATFORM = "all" ] || [ $PLATFORM = $flavour ]; then
     echo "Building $flavour..."
-    docker build -t vips-dev-$flavour platforms/$flavour
-    docker run --rm -e "VERSION_VIPS=${VERSION_VIPS}" -e VERSION_LATEST_REQUIRED -v $PWD:/packaging vips-dev-$flavour sh -c "/packaging/build/lin.sh"
+    if [[ "$flavour" == "linux-arm64v8" ]]; then
+      docker build --platform linux/arm64/v8 -t vips-dev-$flavour platforms/$flavour
+      docker run --platform linux/arm64/v8 --rm -e "VERSION_VIPS=${VERSION_VIPS}" -e VERSION_LATEST_REQUIRED -v $PWD:/packaging vips-dev-$flavour sh -c "/packaging/build/lin.sh"
+    else
+      docker build -t vips-dev-$flavour platforms/$flavour
+      docker run --rm -e "VERSION_VIPS=${VERSION_VIPS}" -e VERSION_LATEST_REQUIRED -v $PWD:/packaging vips-dev-$flavour sh -c "/packaging/build/lin.sh"
+    fi
   fi
 done
